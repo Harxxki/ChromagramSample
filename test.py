b83c08f188aa6b8265075a7ac5c0ab9952664e22
x2fz <haruki112mori@gmail.com>
2020-01-28 08:47:57 +0900

    キー解析のテストまで作った

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 #coding:utf-8
 from collections import OrderedDict
-import os
-import sys
-import numpy as np
 import pydub as dub
 from pydub.playback import play
-import matplotlib.pyplot as plt
+from pprint import pprint
+from glob import glob
+import os
+import sys
 import shutil
-import tempfile as temp
 import librosa
 import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+import tempfile as temp
 import functions as fn
 import soundfile as sf
-from pprint import pprint
-
 
 # 参考:https://qiita.com/yuuki__/items/4bc16ae439de46cd0d76
 class TransToWav:
@@ -23,7 +23,8 @@ class TransToWav:
         self.split_path = os.path.splitext(self.path)
         self.ext = self.split_path[-1]
         self.file_name = _path
-        self.save_file = tmp.name +"/"+ self.file_name + ".wav"
+        # self.save_file = tmp.name +"/"+ self.file_name + ".wav"
+        self.save_file = tmp.name +"/"+ os.path.splitext(self.file_name)[0] + ".wav"
 
     def trans_wav(self):
         self.music = dub.AudioSegment.from_mp3(self.path)
@@ -50,7 +51,6 @@ class WavSaveTmp:
         else:
             pass
 
-
     def save_tmp(self):
         if type(self.file_name) is list:
             for i in self.file_name:
@@ -60,7 +60,7 @@ class WavSaveTmp:
             handle_wav = TransToWav(self.dir_path,self.file_name)
             handle_wav.save_wav()
 
-class play_music:
+class PlayMusic:
 
     def __init__(self):
         self.wav_name = os.listdir(tmp.name)
@@ -73,17 +73,28 @@ class play_music:
             play(self.sound)
         self.play()
 
+class Analyse:
 
-class  Analyse:
     def __init__(self):
         self.dir_path = tmp.name
         self.file_names = os.listdir(self.dir_path)
         self.file_path = [self.dir_path + "/" + i for i in self.file_names]
         self.bpm = {}
         self.key = {}
-        self.chroma = np.zeros(12)
-        self.chromaList = {}
-
+        self.chroma = {}
+        # スケールのテンプレートベクトル
+        # メジャーとマイナーを区別しないダイアトニックスケールのみ(メジャースケールのみの12キー)を考える
+        # 順番を保ちたいのでOrderdDict
+        # TODO: 効率化
+        self.scale_dic = OrderedDict()
+        self.scale = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
+        for i in range(len(self.scale)):
+                self.scale_dic[self.scale[i]] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+                self.scale_dic[self.scale[i]][(i+1)%12] = 0
+                self.scale_dic[self.scale[i]][(i+3)%12] = 0
+                self.scale_dic[self.scale[i]][(i+6)%12] = 0
+                self.scale_dic[self.scale[i]][(i+8)%12] = 0
+                self.scale_dic[self.scale[i]][(i+10)%12] = 0
 
     def analyse_bpm(self):
         for file_name,path in zip(self.file_names,self.file_path):
@@ -102,16 +113,16 @@ class  Analyse:
 
     def calc_chroma(self):
         for file_name,path in zip(self.file_names,self.file_path):
-            if file_name not in self.chromaList:
+            if file_name not in self.chroma:
                 # 曲の1:00~1:30を抜き出す(処理が重い)
                 # TODO: 曲の中心30秒にする
                 self.music, self.sr = librosa.load(path,offset=60.0, duration=30.0)
-                self._bpm, self._beatsPosition = librosa.beat.beat_track(self.music,self.sr)
                 # 楽音成分とパーカッシブ成分に分離
                 harmonic, percussive = librosa.effects.hpss(self.music)
                 # フレームごとのChromaを計算
-                allChroma = librosa.feature.chroma_cens(y=harmonic)
-
+                self.allChroma = librosa.feature.chroma_cens(y=harmonic)
+                self._chroma = np.zeros(12)
+                '''
                 # プロット
                 plt.figure(figsize=(12,4))
                 librosa.display.specshow(allChroma, sr=self.sr, x_axis='time', y_axis='chroma', vmin=0, vmax=1)
@@ -119,19 +130,69 @@ class  Analyse:
                 plt.colorbar()
                 plt.tight_layout()
                 plt.show()
+                '''
                 # フレームごとのChromaを1次元12音階に押し込む
-                for i in range(allChroma.shape[0]):
-                    for j in range(allChroma.shape[1]):
-                        self.chroma[i] += allChroma[i][j]
-                self.chromaList[file_name] = self.chroma
+                for i in range(self.allChroma.shape[0]):
+                    for j in range(self.allChroma.shape[1]):
+                        self._chroma[i] += self.allChroma[i][j]
+                self.chroma[file_name] = self._chroma
             else:
                 continue
-        return self.chromaList
+        return self.chroma
 
-#    def analyse_key(self):
+    def analyse_key(self):
+        for file_name,path in zip(self.file_names,self.file_path):
+            if file_name not in self.chroma:
+                self.music, self.sr = librosa.load(path,offset=60.0, duration=30.0)
+                harmonic, percussive = librosa.effects.hpss(self.music)
+                self.allChroma = librosa.feature.chroma_cens(y=harmonic)
+                self._chroma = np.zeros(12)
+                for i in range(self.allChroma.shape[0]):
+                    for j in range(self.allChroma.shape[1]):
+                        self._chroma[i] += self.allChroma[i][j]
+            # Chromaとコサイン類似度が最大になるスケールを調べる
+            # TODO : 調を決定せずにコサイン類似度を楽曲間類似度に反映させた方がいいか検討する
+            self.maximum = -100000
+            self._key = ""
+            for scale_index, (name, templateVec) in enumerate(self.scale_dic.items()):
+                self.similarity = fn.cos_sim(self._chroma, templateVec)
+                if self.similarity > self.maximum:
+                    self.maximum = self.similarity
+                    self._key = name
+                    self.key[file_name] = self._key
+                else:
+                    continue
+            else:
+                continue
+        return self.key
+
+def keyTest():
+    # [曲名 - (求めたキー,正解のキー,Y/Nラベル)]
+    list = {}
+    actualKeys = {}
+    key_list = analyser.analyse_key()
+    # actualKeysの生成(+キーの変換)
+    for file in glob(path + "/key" + '/*.txt'):
+        f = open(file)
+        basename = os.path.splitext(os.path.basename(file))[0]+".wav"
+        # convertKeyDict = {}
+        # actualKeys[basename] = convertKeyDict[f.readline()]
+        actualKeys[basename] = f.readline()
+        f.close()
+    for i,(song_name,analyzedKey) in enumerate(key_list.items()):
+        actualKey = actualKeys[song_name]
+        if analyzedKey == actualKey:
+            result = "Y"
+        else:
+            result = "N"
+        list[song_name] = (analyzedKey,actualKey,result)
+        # list[song_name.rjust(25)] = list.pop(song_name)
+        list["song"+str(i)] = list.pop(song_name)
+    pprint(list)
 
 ### 前準備
 # path
+# audios/GiantSteps+\ EDM\ Key\ Dataset/sample/audio
 path = sys.argv[1]
 # make temp directory
 tmp = temp.TemporaryDirectory()
@@ -140,58 +201,42 @@ tmp = temp.TemporaryDirectory()
 save_ = WavSaveTmp(path)
 save_.save_tmp()
 
+# instantiation analyser
+analyser = Analyse()
+
+keyTest()
+
 ### BPMと開始拍位置を求める
 # 参考 : https://qiita.com/yuuki__/items/4bc16ae439de46cd0d76
-
 # bpm analyse
-analyser = Analyse()
 bpm_list = analyser.analyse_bpm()
+# print
 pprint(bpm_list)
 
+'''
 ### クロマグラムを求める
 # 参考 : https://qiita.com/namaozi/items/31ea255ecc6a04320dfc
-
 # chroma calc
 chroma_list = analyser.calc_chroma()
+# Normalize
+pprint(chroma_list)
+for chroma in chroma_list:
+    Normalizer = np.linalg.norm(chroma)
+    chroma /= Normalizer
+# print
 pprint(chroma_list)
 
 ### 調性を求める
-# スケールのテンプレートベクトル
-# メジャーとマイナーを区別しないダイアトニックスケールのみ(メジャースケールのみの12キー)を考える
-# 順番を保ちたいのでOrderdDict
-# TODO: 効率化
-scale_dic = OrderedDict()
-scale = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
-for i in range(len(scale)):
-        scale_dic[scale[i]] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-        scale_dic[scale[i]][(i+1)%12] = 0
-        scale_dic[scale[i]][(i+3)%12] = 0
-        scale_dic[scale[i]][(i+6)%12] = 0
-        scale_dic[scale[i]][(i+8)%12] = 0
-        scale_dic[scale[i]][(i+10)%12] = 0
-
-# Normalize
-Normalizer = np.linalg.norm(Chroma)
-Chroma /= Normalizer
-Normalizer = np.linalg.norm(scale_dic["C"])
-for i in range(len(scale)):
-        scale_dic[scale[i]] /= Normalizer
-
-# Chromaとコサイン類似度が最大になるスケールを調べる
-# TODO : 調を決定せずにコサイン類似度を楽曲間類似度に反映させた方がいい...?
-maximum = -100000
-matchedScale = ""
-# result = np.zeros(12)
-for scale_index, (name, vector) in enumerate(scale_dic.items()):
-    similarity = fn.cos_sim(Chroma, vector)
-    # result[scale_index] = similarity
-    if similarity > maximum:
-        maximum = similarity
-        matchedScale = name
-print(name)
-
+# 参考 : https://qiita.com/namaozi/items/31ea255ecc6a04320dfc
+# key analyse
+key_list = analyser.analyse_key()
+# print
+pprint(key_list)
+'''
 
 ### 楽曲間類似度を求め曲順を決定する
+
+### 曲を再生する
 
 ### 後処理
 # clean up temp directory
